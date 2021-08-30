e560099c76748029e3bf52aa6a690e190a74558f
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2021-08-30 22:55:52 +0100

    http -. https

--- src/app/environments/environment.keys.ts
+++ src/app/environments/environment.keys.ts
@@ -1,7 +1,7 @@
 
 export const environmentKeys = {
   weatherApiKey: "4622f4f2d2ce1006ca62e3e0e9e42d97",
-  weatherBaseApiUrl: "http://api.openweathermap.org/data/2.5/"
+  weatherBaseApiUrl: "https://api.openweathermap.org/data/2.5/"
 };
 
 
